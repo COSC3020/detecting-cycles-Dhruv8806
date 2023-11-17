6643f1c6243cfee61d13af0b0c6352a8ff9914da
Dhruv8806 <123929419+Dhruv8806@users.noreply.github.com>
2023-11-17 01:55:42 -0700

    Partial attempt - Work in progress
    I'm using the depth first approach to check for cycles in graph. Let me know if this is the right way to go, or need any changes. ~Thank you

--- code.js
+++ code.js
@@ -1,3 +1,25 @@
 function hasCycle(graph) {
+    const visited = new Array(graph.length).fill(false);
+
+    function hasCycleDFS(node, parent) {
+        visited[node] = true;
+        for (const neighbor of graph[node]) {
+            if (!visited[neighbor]) {
+                if (hasCycleDFS(neighbor, node)) {
+                    return true;
+                }
+            } else if (neighbor !== parent) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    for (let node = 0; node < graph.length; node++) {
+        if (!visited[node] && hasCycleDFS(node, -1)) {
+            return true;
+        }
+    }
+
     return false;
 }
