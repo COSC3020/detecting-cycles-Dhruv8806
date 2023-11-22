be426eb38b899b7f1499aebc4216c621f14309d6
Dhruv8806 <123929419+Dhruv8806@users.noreply.github.com>
2023-11-21 19:01:15 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,25 +1,34 @@
 function hasCycle(graph) {
     const visited = new Array(graph.length).fill(false);
 
-    function hasCycleDFS(node, parent) {
+    // This function is to Recursively perform depth-first search with detecting cycles 
+    function detectCycle(node, parent) {
         visited[node] = true;
+
         for (const neighbor of graph[node]) {
             if (!visited[neighbor]) {
-                if (hasCycleDFS(neighbor, node)) {
+                if (detectCycle(neighbor, node)) {
                     return true;
                 }
             } else if (neighbor !== parent) {
                 return true;
             }
         }
+
         return false;
     }
 
     for (let node = 0; node < graph.length; node++) {
-        if (!visited[node] && hasCycleDFS(node, -1)) {
+        if (!visited[node] && detectCycle(node, -1)) {
             return true;
         }
     }
 
     return false;
 }
+
+/*
+Sources Used:
+https://www.youtube.com/watch?v=9twcmtQj4DU&t=952s
+https://www.geeksforgeeks.org/detect-cycle-in-a-graph/
+*/
